aa311a2aa28cf71d344237c90e74dce34ce8854f
popzlat <45499958+popzlat@users.noreply.github.com>
2019-01-05 17:06:05 +0100

    Update script.js

--- Exercise.JS/range/script.js
+++ Exercise.JS/range/script.js
@@ -6,3 +6,8 @@ let range = (start, end) => {
     const length = end - start;
     return Array.from({ length }, (_, i) => start + i);
 }
+
+// Test 
+console.log(range(20,30));
+console.log(range(10,40));
+console.log(range(55,65));
